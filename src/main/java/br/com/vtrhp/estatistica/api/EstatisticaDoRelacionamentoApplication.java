84192134aca7d3b9b4d8507ac362a0972c2eaa12
Victor Possidonio <vtrhp@hotmail.com>
2019-04-13 11:45:29 -0300

    Commit inicial dos arquivos do Projeto

---
+++ src/main/java/br/com/vtrhp/estatistica/api/EstatisticaDoRelacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vtrhp.estatistica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatisticaDoRelacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatisticaDoRelacionamentoApplication.class, args);
+	}
+
+}
